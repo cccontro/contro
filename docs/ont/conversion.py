72e162caf30e86e92941355c506421d1289b0978
Francesca Massarenti <146438454+frammenti@users.noreply.github.com>
2025-03-19 19:01:00 +0100

    Finished ontology conversion

--- docs/ont/conversion.py
+++ docs/ont/conversion.py
@@ -1,50 +1,142 @@
-from rdflib import Graph, Namespace, URIRef, RDF
+from rdflib import Graph, Namespace, URIRef, Literal, RDF, RDFS, PROV, OWL
 import xml.etree.ElementTree as ET
 
-# Define the namespaces
-ARG = Namespace("https://w3id.org/contro/arg#")
-RDF_NS = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+# RDF graph
+ARG = Namespace('https://w3id.org/contro/arg#')
+CONTRO = Namespace('https://w3id.org/contro#')
+DUL = Namespace('http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#')
+g = Graph(bind_namespaces='rdflib')
+g.bind('contro', CONTRO)
+g.bind('arg', ARG)
+g.bind('dul', DUL)
 
-# Initialize RDF Graph
-g = Graph()
-g.bind("arg", ARG)
-g.bind("rdf", RDF_NS)
+# XML namespaces
+ns = {'xml': 'http://www.w3.org/XML/1998/namespace',
+      'tei': 'http://www.tei-c.org/ns/1.0'}
+id = '{http://www.w3.org/XML/1998/namespace}id'
+lang = '{http://www.w3.org/XML/1998/namespace}lang'
 
-# Parse the TEI XML file
-tei_xml = """<list type="syllogism"
-    xmlns:arg="https://w3id.org/contro/arg#"
-    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
-  <item xml:id="S1" rdf:type="arg:Premise"/>
-  <item xml:id="S2" rdf:type="arg:Premise"/>
-  <item xml:id="S3" rdf:type="arg:Premise" arg:Antecedent="#S1 #S2" arg:Consequent="#S4"/>
-  <item xml:id="S4" rdf:type="arg:Conclusion"/>
-</list>"""
+tree = ET.parse('tei/Apologia-TEI.xml')
+root = tree.getroot()
 
-# Parse XML
-root = ET.fromstring(tei_xml)
+# Pre-compute a parent lookup, mapping each element to its parent
+par = {child: parent for parent in root.iter() for child in parent}
 
-# Process each <item>
-for item in root.findall("item"):
-    item_id = item.get("{http://www.w3.org/XML/1998/namespace}id")  # Get xml:id
-    item_uri = URIRef(f"https://w3id.org/contro/arg#{item_id}")  # Create RDF URI
+# Find the closest ancestor div with xml:lang
+def get_lang(el):
+    while el in par:
+        el = par[el] # El is now the parent
+        if el.tag == '{http://www.tei-c.org/ns/1.0}div' and lang in el.attrib:
+            return el.get(lang)
+    return 'it'
 
-    # Assign rdf:type
-    rdf_type = item.get("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}type")
-    if rdf_type:
-        g.add((item_uri, RDF.type, URIRef(rdf_type)))
+cat_xpath = './/tei:taxonomy[@xml:id="{}"]/tei:category'
 
-    # Handle Antecedent(s)
-    antecedents = item.get("{https://w3id.org/contro/arg#}Antecedent")
-    if antecedents:
-        for ant in antecedents.split():
-            ant_uri = URIRef(f"https://w3id.org/contro/arg#{ant.strip('#')}")
-            g.add((item_uri, ARG.Antecedent, ant_uri))
+# Dictionary of category ids to property names
+# e.g. { 'premise': 'Premise' }
+tx = {cat.get(id): cat.find('tei:catDesc', ns).text for cat in root.findall(cat_xpath.format('statement'), ns)}
 
-    # Handle Consequent(s)
-    consequent = item.get("{https://w3id.org/contro/arg#}Consequent")
-    if consequent:
-        cons_uri = URIRef(f"https://w3id.org/contro/arg#{consequent.strip('#')}")
-        g.add((item_uri, ARG.Consequent, cons_uri))
+# Topics
+for topic in root.findall(cat_xpath.format('topic'), ns):
+    topic_id = topic.get(id)
+    topic_uri = CONTRO[topic_id]
 
-# Serialize RDF output (Turtle format)
-print(g.serialize(format="turtle").decode())
+    g.add((topic_uri, RDF.type, ARG.Topic))
+
+    descs = topic.findall('tei:catDesc', ns)
+    langs = {desc.get(lang) for desc in descs if desc.get(lang)}
+
+    if langs:
+        for l in langs:
+            lang_descs = filter(lambda x: x.get(lang) == l, descs)
+            topic_title = next(lang_descs).text
+            g.add((topic_uri, RDFS.label, Literal(topic_title, lang=l)))
+            for topic_desc in lang_descs:
+                g.add((topic_uri, RDFS.comment, Literal(topic_desc.text, lang=l)))
+    else:
+        topic_title = descs[0].text
+        g.add((topic_uri, RDFS.label, Literal(topic_title, lang='en')))
+        for topic_desc in descs[1:]:
+            g.add((topic_uri, RDFS.comment, Literal(topic_desc.text, lang='en')))
+
+    # Topic instances
+    instances = root.findall(f".//tei:seg[@ana='#{topic_id}']", ns)
+
+    if instances:
+        g.add((topic_uri, RDF.type, DUL.Collection))
+
+    for instance in instances:
+        instance_id = instance.get(id)
+
+        if not instance_id:
+            continue
+        
+        instance_uri = CONTRO[instance_id]
+        g.add((topic_uri, DUL.hasMember, instance_uri))
+        g.add((instance_uri, RDFS.label, Literal(instance.text, lang='it')))
+
+# Authors
+def add_person_details(target_id):
+    target = root.find(f".//tei:particDesc//tei:person[@xml:id='{target_id}']", ns)
+    if target is None:
+        person = root.find(f".//tei:particDesc//tei:persona[@xml:id='{target_id}']/..", ns)
+    else:
+        person = target
+    person_uri = CONTRO[person.get(id)]
+    g.add((person_uri, RDF.type, DUL.Agent))
+    g.add((person_uri, OWL.sameAs, URIRef(person.get('sameAs'))))
+    g.add((person_uri, RDFS.label, Literal(person.find('tei:persName', ns).text)))
+
+    for persona in person.findall('tei:persona', ns):
+        persona_uri = CONTRO[persona.get(id)]
+        g.add((persona_uri, RDF.type, DUL.Agent))
+        g.add((persona_uri, RDFS.label, Literal(persona.find('tei:persName', ns).text)))
+        note = persona.find('tei:note', ns)
+        if note is not None:
+            g.add((persona_uri, RDFS.comment, Literal(note.text, lang='en')))
+        g.add((persona_uri, ARG.personaOf, person_uri))
+
+# Arguments
+for context in root.findall(".//tei:note[@type='paraphrase']/..", ns): # Get parent div with responsability info
+    author = context.get('resp')[1:]
+
+    for arg in context.findall("./tei:note[@type='paraphrase']/tei:list[@type='syllogism']", ns):
+        arg_id = arg.get(id)
+        arg_uri = CONTRO[arg_id]
+        g.add((arg_uri, RDF.type, ARG.Argument))
+        g.add((arg_uri, ARG.Author, CONTRO[author]))
+        add_person_details(author)
+        g.add((arg_uri, ARG.Topic, CONTRO[arg.get('ana')[1:]]))
+
+        for stmt in arg.findall('tei:item', ns):
+            # If the statement has no id, it is a reference to another statement
+            if not stmt.get(id):
+                stmt_uri = CONTRO[stmt.find('tei:ptr', ns).get('target')[1:]]
+                g.add((arg_uri, ARG[tx[stmt.get('ana')[1:]]], stmt_uri))
+                continue
+
+            stmt_id = stmt.get(id)
+            stmt_uri = CONTRO[stmt_id]
+            g.add((arg_uri, ARG[tx[stmt.get('ana')[1:]]], stmt_uri))
+            g.add((stmt_uri, RDFS.label, Literal(stmt.text, lang='en')))
+
+            # Inference Rule components (if any)
+            antecedents = stmt.get('synch')
+            if antecedents:
+                for antecedent in antecedents.split():
+                    g.add((stmt_uri, ARG.Antecedent, CONTRO[antecedent[1:]]))
+                g.add((stmt_uri, ARG.Consequent, CONTRO[stmt.get('next')[1:]]))
+
+            # Contrary
+            contrary = stmt.get('exclude')
+            if contrary:
+                g.add((stmt_uri, ARG.contradicts, CONTRO[contrary[1:]]))
+            
+            # Text content
+            for text in root.findall(f".//*[@ana='#{stmt_id}']", ns):
+                text_lang = get_lang(text)
+                full_text = ''.join(text.itertext()).strip()
+                g.add((stmt_uri, PROV.value, Literal(full_text, lang=text_lang)))
+
+with open('ont/contro.ttl', 'w') as f:
+    f.write(g.serialize(format='turtle'))
