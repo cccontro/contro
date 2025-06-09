d95deda6421208be6b927f1e95e96f525e82ba9a
Francesca Massarenti <146438454+frammenti@users.noreply.github.com>
2025-06-09 06:03:21 +0200

    Deployed 2205f39 with MkDocs version: 1.6.1

---
+++ assets/javascripts/hero.js
@@ -0,0 +1,11 @@
+// Function to set the CSS variable
+function setHeroHeight() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+// Subscribe on load and on resize events
+document$.subscribe(() => {
+  setHeroHeight();
+  window.addEventListener('resize', setHeroHeight);
+});
